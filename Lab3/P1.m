6c1432354176293a5fe28e3eb4cb494c902c29a7
hpetiot <hm.petiot@gmail.com>
2022-10-11 12:21:25 +0200

    fixed translation of points

--- Lab3/P1.m
+++ Lab3/P1.m
@@ -3,7 +3,7 @@ imageBuss = imread('bus.jpeg');
 imageSimpson = imread('simpsons.jpeg');
 
 busDisplayCorners  = [583 209; 798 229; 594 520; 807 468];
-simpsonCorners = [1 1; 1 1000; 1500 1; 1500 1000];
+simpsonCorners = [1 1; 1000 1; 1 1500; 1000 1500];
 #(TL TR BL BR) where desided by zooming in to see the actual pixel and selecting the corners looking hte more like the corners of the display.
 TopAfine_A = (busDisplayCorners(2, 2) - busDisplayCorners(1, 2))/(busDisplayCorners(2, 1)-busDisplayCorners(1,1));
 TopAfine_B = busDisplayCorners(1, 2) - TopAfine_A*busDisplayCorners(1, 1);
@@ -11,7 +11,7 @@ busCpy = imageBuss;
 EquMat = [
           simpsonCorners(1, 1) simpsonCorners(1, 2) 1 0 0 0 -(simpsonCorners(1, 1) * busDisplayCorners(1, 1)) -(simpsonCorners(1, 2) * busDisplayCorners(1, 1));
           0 0 0 simpsonCorners(1, 1) simpsonCorners(1, 2) 1 -simpsonCorners(1, 1)*busDisplayCorners(1, 2) -(simpsonCorners(1, 2) * busDisplayCorners(1, 2));
-          simpsonCorners(2, 1) simpsonCorners(2, 2) 1 0 0 0 -(simpsonCorners(1, 1) * busDisplayCorners(2, 1)) -(simpsonCorners(2, 2) * busDisplayCorners(2, 1));
+          simpsonCorners(2, 1) simpsonCorners(2, 2) 1 0 0 0 -(simpsonCorners(2, 1) * busDisplayCorners(2, 1)) -(simpsonCorners(2, 2) * busDisplayCorners(2, 1));
           0 0 0 simpsonCorners(2, 1) simpsonCorners(2, 2) 1 -simpsonCorners(2, 1)*busDisplayCorners(2, 2) -(simpsonCorners(2, 2) * busDisplayCorners(2, 2));
           simpsonCorners(3, 1) simpsonCorners(3, 2) 1 0 0 0 -(simpsonCorners(3, 1) * busDisplayCorners(3, 1)) -(simpsonCorners(3, 2) * busDisplayCorners(3, 1));
           0 0 0 simpsonCorners(3, 1) simpsonCorners(3, 2) 1 -simpsonCorners(3, 1)*busDisplayCorners(3, 2) -(simpsonCorners(3, 2) * busDisplayCorners(3, 2));
@@ -33,17 +33,21 @@ T = [
     a(1) a(2) a(3);
     a(4) a(5) a(6);
     a(7) a(8) 1;
-];
+]
 
-for i=simpsonCorners(1, 1):simpsonCorners(2, 1)
-    for j = simpsonCorners(1, 2):simpsonCorners(3, 2)
-        coordSimp = [i; j; 1];
-        transCoordSimpson = T*coordSimp;
-        busCpy(ceil(coordBus(1)), ceil(coordBus(2)), 1) = imageSimpson(i, j, 1);
-        busCpy(ceil(coordBus(1)), ceil(coordBus(2)), 2) = imageSimpson(i, j, 2);
-        busCpy(ceil(coordBus(1)), ceil(coordBus(2)), 3) = imageSimpson(i, j, 3);
-
-    endfor
-endfor
-imshow(busCpy, []);
-whos;
+figure("name", "test transform B->S");
+TL = pinv(T) * [busDisplayCorners(1, 1); busDisplayCorners(1, 2); 1];
+TR = pinv(T) * [busDisplayCorners(2, 1); busDisplayCorners(2, 2); 1];
+BL = pinv(T) * [busDisplayCorners(3, 1); busDisplayCorners(3, 2); 1];
+BR = pinv(T) * [busDisplayCorners(4, 1); busDisplayCorners(4, 2); 1];
+TL = [TL(1) / TL(3);TL(2)/TL(3); 1];
+TR = [TR(1) / TR(3);TR(2)/TR(3); 1];
+BR = [BR(1) / BR(3);BR(2)/BR(3); 1];
+BL = [BL(1) / BL(3);BL(2)/BL(3); 1];
+hold("on");
+imshow(imageSimpson, []);
+plot(TL(1), TL(2), "ro");
+plot(TR(1), TR(2), "ro");
+plot(BL(1), BL(2), "ro");
+plot(BR(1), BR(2), "ro");
+hold("off");
