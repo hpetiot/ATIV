4f59c8701b96909438334418876254bd15b5747a
hpetiot <hm.petiot@gmail.com>
2022-10-21 17:52:56 +0200

    zorg!

--- Lab3/P2/test.py
+++ Lab3/P2/test.py
@@ -28,16 +28,28 @@ def usingSift(keble_a, keble_b, keble_c):
     return featureA, featureB, featureC
 
 def usingHarris(keble_a, keble_b, keble_c):
+    # as this method does not yield point coordinates, I will not use it in the rest of the TP.
     grey_a = openCV.cvtColor(keble_a, openCV.COLOR_BGR2GRAY)
     grey_b = openCV.cvtColor(keble_b, openCV.COLOR_BGR2GRAY)
     grey_c = openCV.cvtColor(keble_c, openCV.COLOR_BGR2GRAY)
-    # /!\ these are not coordinates.!!
+   
     harris_a = openCV.cornerHArris(grey_a, 4, 3, 0.04)
     harris_b = openCV.cornerHArris(grey_a, 4, 3, 0.04)
     harris_c = openCV.cornerHArris(grey_a, 4, 3, 0.04)
 
+def strangeMatch(descriptor_one, descriptor_two):
+    matches = openCV.bf.knnMatch(descriptor_one, descriptor_two, k=2)
+    inliers = [];
+    outliers = [];
+    for m,n in matches:
+        if m.distance < 0.75*n.distance:
+            inliers.append((m, n))
+        else:
+            outliers.append((m,n))
+    return (inliers, outliers)
 
 
+def ransac(des1, des2):
 
 # question @teach' : 
 #   -> witch part are we supposed to actualy code ourselves (feature finding, feature matching, Ransack)
