0c97011aa749e34c4038b92c84a7f16dc72b573e
hpetiot <hm.petiot@gmail.com>
2022-10-25 10:58:25 +0200

    added l7 + finished HW

--- Lab3/P2/test.py
+++ Lab3/P2/test.py
@@ -1,30 +1,36 @@
 from xmlrpc.client import FastMarshaller
 import cv2 as openCV
+import numpy as np
+from numpy import Infinity
 
-keble_a = openCV.imread("Introduction_to_CV/Lab3/keble/keble_a.jpg");
-keble_b = openCV.imread("Introduction_to_CV/Lab3/keble/keble_b.jpg");
-keble_c = openCV.imread("Introduction_to_CV/Lab3/keble/keble_c.jpg");
+keble_a = openCV.imread("Introduction_to_CV/Lab3/keble/keble_a.jpg")
+keble_b = openCV.imread("Introduction_to_CV/Lab3/keble/keble_b.jpg")
+keble_c = openCV.imread("Introduction_to_CV/Lab3/keble/keble_c.jpg")
+
+grey_a = openCV.imread("/home/hugo/Desktop/ATIV/Introduction_to_CV/Lab3/keble/keble_a.jpg", openCV.IMREAD_GRAYSCALE)
+grey_b = openCV.imread("/home/hugo/Desktop/ATIV/Introduction_to_CV/Lab3/keble/keble_a.jpg", openCV.IMREAD_GRAYSCALE)
+grey_c = openCV.imread("/home/hugo/Desktop/ATIV/Introduction_to_CV/Lab3/keble/keble_a.jpg", openCV.IMREAD_GRAYSCALE)
 
 #using FAST
-def usingFast(keble_a, keble_b, keble_c):
+def usingFast(a, b, c):
     fastA = openCV.FastFeatureDetector_create()
     fastB = openCV.FastFeatureDetector_create()
     fastC = openCV.FastFeatureDetector_create()
 
-    featureA = fastA.detect(keble_a, None)
-    featureB = fastB.detect(keble_b, None)
-    featureC = fastC.detect(keble_c, None)
+    featureA = fastA.detect(a, None)
+    featureB = fastB.detect(b, None)
+    featureC = fastC.detect(c, None)
 
     return featureA, featureB, featureC
 
-def usingSift(keble_a, keble_b, keble_c):
+def usingSift(a, b, c):
     siftA = openCV.SIFT_create()
     siftB = openCV.SIFT_create()
     siftC = openCV.SIFT_create()
 
-    featureA = siftA.detect()
-    featureB = siftB.detect()
-    featureC = siftC.detect()
+    featureA = siftA.detectAndCompute(a, None)
+    featureB = siftB.detectAndCompute(b, None)
+    featureC = siftC.detectAndCompute(c, None)
     return featureA, featureB, featureC
 
 def usingHarris(keble_a, keble_b, keble_c):
@@ -37,19 +43,22 @@ def usingHarris(keble_a, keble_b, keble_c):
     harris_b = openCV.cornerHArris(grey_a, 4, 3, 0.04)
     harris_c = openCV.cornerHArris(grey_a, 4, 3, 0.04)
 
-def strangeMatch(descriptor_one, descriptor_two):
-    matches = openCV.bf.knnMatch(descriptor_one, descriptor_two, k=2)
-    inliers = [];
-    outliers = [];
-    for m,n in matches:
-        if m.distance < 0.75*n.distance:
-            inliers.append((m, n))
-        else:
-            outliers.append((m,n))
-    return (inliers, outliers)
+def matchAndRansac(descriptor_one, descriptor_two, kp1, kp2):
+    matches = openCV.BFMatcher().knnMatch(descriptor_one, descriptor_two, k=2)
+    left = np.float32([kp1[i].pt for i in range(0, len(kp1))]).reshape(-1, 1, 2)
+    right = np.float32([kp2[i].pt for i in range(0, len(kp2))]).reshape(-1, 1, 2)
 
+    M, mask = openCV.findHomography(left, right, openCV.RANSAC, 5.0)
+    return M
 
-def ransac(des1, des2):
+fA, fB, fC = usingSift(grey_a, grey_b, grey_c)
+kpA, descA = fA
+kpB, descB = fB
+kpC, descC = fC
+M_AtoB = matchAndRansac(descA, descB, kpA, kpB)
+M_CtoB = matchAndRansac(descC, descB, kpC, kpB)
+print(M_AtoB)
+print(M_CtoB)
 
 # question @teach' : 
 #   -> witch part are we supposed to actualy code ourselves (feature finding, feature matching, Ransack)
